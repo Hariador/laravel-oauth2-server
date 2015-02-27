853c5fca293279c222038c1fd326635379796245
Alexander Trauzzi <atrauzzi@gmail.com>
2015-02-27 13:15:42 -0600

    Up you go!

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php namespace Atrauzzi\LaravelOauth2Server {
+
+	use Illuminate\Support\ServiceProvider as Base;
+
+
+	class ServiceProvider extends Base {
+
+		/**
+		 * Register the service provider.
+		 *
+		 * @return void
+		 */
+		public function register() {
+			// TODO: Implement register() method.
+		}
+
+	}
+
+}
