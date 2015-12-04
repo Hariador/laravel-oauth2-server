1a45fdec6d61c2debd4292ee94d8c96b9123f52a
Jonathan Reichert <jonathan@boldcommerce.com>
2015-12-04 10:29:16 -0600

    Small fix to AccessToken::persist to make the datime conversion from epoch to timestring work

--- src/Domain/Repository/Cache/AccessToken.php
+++ src/Domain/Repository/Cache/AccessToken.php
@@ -21,7 +21,10 @@
 		 * @return \Atrauzzi\LaravelOauth2Server\Domain\Entity\AccessToken
 		 */
 		public function create($id, $expireTime, $oauthableId, $oauthableType, $clientId, array $scopeNames) {
-			return new AccessTokenEntity($id, $expireTime, $oauthableId, $oauthableType, $clientId, $scopeNames);
+			error_log("Passing along constructor call",0);
+			$temp= new AccessTokenEntity($id, $expireTime, $oauthableId, $oauthableType, $clientId, $scopeNames);
+
+			return $temp ;
 		}
 
 		/**
@@ -39,10 +42,12 @@
 		 * @param \Atrauzzi\Oauth2Server\Domain\Entity\AccessToken $accessToken
 		 */
 		public function persist(AccessTokenContract $accessToken) {
+
+			$epoch_time = $accessToken->getExpireTime();
 			$this->cache->put(
 				$this->getKey('access_token', $accessToken->getId()),
 				$accessToken,
-				new DateTime($accessToken->getExpireTime())
+				new DateTime("@$epoch_time")
 			);
 		}
 
